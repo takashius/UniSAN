bd7bfbf9224bc833c2c68dfa965678c2b3a2ae3f
Erick Hernandez <takashi.onimaru@gmail.com>
2025-04-07 21:44:16 -0400

    Refactor code for improved performance

---
+++ src/services/auth.ts
@@ -0,0 +1,92 @@
+import {
+  Register,
+  useMutation,
+  useQuery,
+  UseQueryResult,
+  UseMutationResult,
+  useQueryClient,
+} from "@tanstack/react-query";
+import ERDEAxios from "./ERDEAxios";
+import { Account, LoginResponse, Login, Image, Recovery } from "../types";
+
+export const useLogin = (): UseMutationResult<
+  LoginResponse,
+  unknown,
+  Login
+> => {
+  return useMutation<LoginResponse, unknown, Login>({
+    mutationFn: async (data: Login) => {
+      const response = await ERDEAxios.post("/user/login", data);
+      return response.data;
+    },
+  });
+};
+
+export const useAccount = (): UseQueryResult<Account, Error> => {
+  return useQuery<Account, Error>({
+    queryKey: ["myAccount"],
+    retry: false,
+    queryFn: () => {
+      return ERDEAxios.get<Account>("/user/account").then(
+        (response) => response.data
+      );
+    },
+  });
+};
+
+export const useLogout = () => {
+  return useMutation({
+    mutationFn: async () => {
+      const response = await ERDEAxios.post("/user/logout");
+      return response.data;
+    },
+  });
+};
+
+export const useRegister = (): UseMutationResult<any, unknown, Register> => {
+  return useMutation<any, unknown, Register>({
+    mutationFn: async (data: Register) => {
+      const response = await ERDEAxios.post("/user/register", data);
+      return response.data;
+    },
+  });
+};
+
+export const useRecoveryOne = () => {
+  return useMutation({
+    mutationFn: (email: String) => {
+      return ERDEAxios.get("/user/recovery/" + email);
+    },
+  });
+};
+
+export const useRecoveryTwo = () => {
+  return useMutation({
+    mutationFn: (data: Recovery) => {
+      return ERDEAxios.post("/user/recovery", data);
+    },
+  });
+};
+
+export const useUploadImage = () => {
+  const queryClient = useQueryClient();
+  const mutation = useMutation({
+    mutationFn: (data: Image) => {
+      localStorage.setItem("contentType", "true");
+      var formData = new FormData();
+      formData.append("image", data.image);
+      formData.append("imageType", data.imageType);
+      return ERDEAxios.post("/company/upload", formData);
+    },
+    onSuccess: () => {
+      localStorage.removeItem("contentType");
+      queryClient.invalidateQueries({ queryKey: ["myAccount"] });
+    },
+    onError: (error) => {
+      console.log("error useUploadImage", error);
+      localStorage.removeItem("contentType");
+    },
+  });
+
+  return mutation;
+};
---
+++ src/types/index.d.ts
@@ -0,0 +1,88 @@
+export interface Created {
+  user: string;
+  date: string;
+}
+
+export interface CreatedExtend {
+  user: User;
+  date: string;
+}
+
+export interface User {
+  _id: string;
+  name: string;
+  lastName: string;
+}
+
+export interface LoginResponse {
+  _id: string;
+  name: string;
+  lastName?: string;
+  photo?: string;
+  email: string;
+  date: string;
+  token: string;
+}
+
+export interface UserLogin {
+  name: string;
+  lastName: string;
+  email: string;
+  token: string;
+  _id: string;
+}
+
+export interface Login {
+  email: string;
+  password: string;
+}
+
+export interface Image {
+  image: any;
+  imageType: string;
+}
+
+export interface Recovery {
+  code: Number;
+  email: string;
+  newPass: string;
+}
+
+export interface UserAccount {
+  id: string;
+  name: string;
+  email: string;
+  points: number;
+  level: number;
+}
+
+export interface Statistics {
+  activeSansCount: number;
+  completedSansCount: number;
+  totalSavings: number;
+  totalPayments: number;
+  onTimePayments: number;
+}
+
+export interface San {
+  sanName: string;
+  amount: number;
+  startDate: string;
+  frequency: string;
+  position: number;
+}
+
+export interface NextPayment {
+  sanName: string;
+  sanAmount: number;
+  paymentAmount: number;
+  nextPaymentDate: string;
+  isOwnTurn: boolean;
+}
+
+export interface Account {
+  user: UserAccount;
+  statistics: Statistics;
+  sans: San[];
+  nextPayments: NextPayment[];
+}
